2911a99a2e2b9de6a162b8e97073e2e3d80fc152
aloha66 <lzcbruce@163.com>
2019-02-20 23:11:17 +0800

    [新增]button组件

---
+++ src/components/button/button.vue
@@ -0,0 +1,32 @@
+<template>
+  <component :is="tagName" v-bind="tagProps"> <slot></slot> </component>
+</template>
+<script>
+export default {
+  props: {
+    to: {
+      type: String,
+      default: ""
+    },
+    target: {
+      type: String,
+      default: "_self"
+    }
+  },
+  computed: {
+    tagName() {
+      return this.to === "" ? "button" : "a";
+    },
+    tagProps() {
+      let props = {};
+      if (this.to) {
+        props = {
+          target: this.target,
+          href: this.to
+        };
+      }
+      return props;
+    }
+  }
+};
+</script>
